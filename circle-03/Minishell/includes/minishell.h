95e31d20642e153ae32cf7a44a960b8950337211
vRubio <vctrubio@gmail.com>
2022-07-02 21:56:16 +0200

    working prompt

--- circle-03/Minishell/includes/minishell.h
+++ circle-03/Minishell/includes/minishell.h
@@ -6,13 +6,14 @@
 /*   By: V <V@student.42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/21 12:38:08 by joaribei          #+#    #+#             */
-/*   Updated: 2022/07/02 11:49:12 by V                ###   ########.fr       */
+/*   Updated: 2022/07/02 21:52:27 by V                ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
+# include <unistd.h>
 # include <stdbool.h>
 # include <stdio.h>
 # include <readline/readline.h>
---
+++ circle-03/Minishell/includes/prompt.h
@@ -0,0 +1,8 @@
+
+#ifndef PROMPT_H
+# define PROMPT_H
+
+# include "string.h"
+char	*ft_prompt(void);
+
+#endif
