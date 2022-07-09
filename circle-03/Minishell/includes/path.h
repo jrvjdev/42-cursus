5e567514b97ac33755389c5261f21eff4149771c
vRubio <vctrubio@gmail.com>
2022-07-09 16:25:48 +0200

    termios not working

---
+++ circle-03/Minishell/includes/path.h
@@ -0,0 +1,9 @@
+
+#ifndef PATH_H
+# define PATH_H
+
+# include "minishell.h"
+
+
+
+#endif
--- circle-03/Minishell/sources/termios/termios.c
+++ circle-03/Minishell/sources/termios/termios.c
@@ -1,40 +1,37 @@
 #include "../../includes/minishell.h"
 
-static void	sig_int(int sig)
+void	sig_int(int sig)
 {
 	if (sig == SIGINT)
 	{
-		write(1, "nl\n", 3);
-		//write(1, "newline\n", 8);
-		//rl_replace_line("", 0);
-		rl_on_new_line();
-		rl_redisplay();
-		//rl_reset_line_state();
-		//write(1, "$>", 2);
-		get_shell()->ec = 130;
+		printf("(newline)\n");
+		// rl_replace_line("", 0);
+		// rl_on_new_line();
+		// rl_redisplay();
 	}
 }
-
-
-
+void	sig_quit(int sig)
+{
+	if (sig == SIGQUIT)
+	{
+		printf("(quit)\n");
+	}
+}
 void	init_termios(void)
 {
-	struct sigaction	sa;
-//	struct sigaction	sa_1;
+	struct sigaction	sa_ctrc;
+	struct sigaction	sa_ctrd;
 	struct termios		term;
 
 	tcgetattr(0, &term);
-	term.c_lflag &= ~ECHOCTL;
+	term.c_lflag &= (~ECHOCTL);
 	tcsetattr(0, TCSANOW, &term);
 
-	sa.sa_handler = &sig_int;
-//	sa_1.sa_handler = SIG_IGN;
-	sa.sa_flags = SA_RESTART;
-	sigaction(SIGINT, &sa, NULL);
-//	sigaction(SIGQUIT, &sa, NULL);
-	
-//	rl_bind_key ('\t', rl_insert);
-	printf("hi termi\n");
+	sa_ctrc.sa_handler = &sig_int;
+	sa_ctrd.sa_handler = &sig_quit;
+	sa_ctrc.sa_flags = SA_RESTART;
+	sigaction(SIGINT, &sa_ctrc, NULL);
+	sigaction(SIGQUIT, &sa_ctrd, NULL);
 }
 
 /* handle
